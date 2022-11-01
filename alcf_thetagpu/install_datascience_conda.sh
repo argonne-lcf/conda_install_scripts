d333e4e3f4650c501a763077ff446b89a6061932
Kyle Gerard Felker <felker@anl.gov>
2022-11-01 15:48:51 -0500

    Add other packages from recent Polaris changes

--- alcf_thetagpu/install_datascience_conda.sh
+++ alcf_thetagpu/install_datascience_conda.sh
@@ -512,8 +512,9 @@ pip install torch-tb-profiler
 pip install torchinfo  # https://github.com/TylerYep/torchinfo successor to torchsummary (https://github.com/sksq96/pytorch-summary)
 pip install cupy-cuda${CUDA_VERSION_MAJOR}${CUDA_VERSION_MINOR}
 pip install pytorch-lightning
-pip install gpytorch
 pip install deepspeed
+pip install gpytorch xgboost multiprocess py4j
+pip install hydra-core hydra_colorlog accelerate arviz pyright celerite seaborn xarray bokeh matplotx aim torchviz rich parse
 
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 #pip install "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
